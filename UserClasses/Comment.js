f9da3b22beaec707ec9c095061462fcd39b5328b
Thomas Ludlow <twjludlow@gmail.com>
2017-11-05 16:34:46 +0000

    asdas

---
+++ UserClasses/Comment.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import User from "../UserClasses/User";
+export class Comment {
+
+    constructor(content, user, job) {
+        this.content = content;
+        this.user = new User();
+        this.postcode = postcode
+    }
+
+
+    
+}
