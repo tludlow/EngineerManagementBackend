92564c9118972eae8d0b2fc8938d3e41802682ba
Thomas Ludlow <twjludlow@gmail.com>
2017-12-02 14:54:56 +0000

    added specific admin inheritance interactions, also added another test to extend the proof of concept

--- UserClasses/Admin.js
+++ UserClasses/Admin.js
@@ -1,7 +1,23 @@
 export class Admin extends User {
     
-    constructor() {
+    constructor(adminLevel) {
         super();
         this.role = "admin"
+        this.adminLevel = adminLevel;
+    }
+
+    set adminLevel(adminLevel) {
+        this.adminLevel = adminLevel;
+    }
+
+    get adminLevel() {
+        return this.adminLevel;
+    }
+
+    verfiyActionWithLevel(levelRequired) {
+        if(levelRequired > this.adminLevel) {
+            return false;
+        }
+        return true;
     }
 }
--- test/test.js
+++ test/test.js
@@ -4,16 +4,24 @@ var should = chai.should();
 var expect = chai.expect();
 var axios = require("axios");
 
-
-
 describe("Job Testing", function() {
-    it("Should return an error as no jwt token taken", function(done) {
+    it("Should return an error as no jwt token given", function(done) {
         axios.get("http://localhost:7770/job/getJobsAndLocations/meow").then((res)=> {
             res.should.have.status(404);
             done();
         }).catch((err)=> {
             res.should.have.status(404);
             done();
+        });
+    });
+
+    it("Should return all jobs as we have provided a token", function(done) {
+        axios.get("http://localhost:7770/job/getJobsAndLocations/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoiVGhvbWFzSXNDb29sIiwiZW1haWwiOiJ0aG9tYXNpc2Nvb2xAdGVzdC5tZSIsInJvbGUiOiJ1c2VyIiwidmVyaWZpZWQiOmZhbHNlfSwiaWF0IjoxNTEyMjI1MjU1fQ.uDoqtYP7tOU5E5ukQPpXhBmHJ24hPJ_vf-hPG5iVqH0").then((res)=> {
+            expect(res).to.be.an("object");
+            expect(res).to.include({ok: true});
+            done();
+        }).catch((err)=> {
+            expect(err).to.be.an("error");
         });
     });
 });
